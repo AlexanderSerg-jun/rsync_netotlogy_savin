49b62b689c861e616cd005c6a99d09067105875e
Alexander Savin <itspecos@gmail.com>
2025-08-30 21:14:08 +0400

    My homework

---
+++ file/backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Что копируем
+SOURCE_DIR="/home/savinas"
+# Куда
+TARGET_DIR="/tmp/backup"
+# Cтандартный вывод - в /dev/null, ошибки - в лог.
+rsync -a --checksum --exclude=".*" "$SOURCE_DIR" "$TARGET_DIR" > /dev/null 2>> /var/log/backup.log
+
+# Проверка кода завершения rsync и запись лога
+if [ $? -eq 0 ]; then
+    echo "[$(date)] Резервное копирование успешно выполнено" >> /var/log/backup.log
+else
+    echo "[$(date)] Ошибка при выполнении резервного копирования" >> /var/log/backup.log
+fi
